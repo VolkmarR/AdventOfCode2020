43beb8fb0c6676e142ff66f84f80f873fd6e11ea
Volkmar Rigo <volkmar.rigo@infominds.eu>
2020-12-10 21:53:20 +0100

    Fixed lost solution of day 8

--- Day08/Day08Solver.cs
+++ Day08/Day08Solver.cs
@@ -15,27 +15,61 @@ namespace Day08
 
         [Fact]
         public void RunStep1() => Output.WriteLine(new Day08Solver().ExecutePuzzle1());
-        
+
         [Fact]
         public void RunStep2() => Output.WriteLine(new Day08Solver().ExecutePuzzle2());
     }
 
     public class Day08Solver : SolverBase
     {
-        // List<??> Data;
+        Calculator Data;
 
         protected override void Parse(List<string> data)
         {
-            // Data = new();
+            Data = new(data);
+        }
+
+        bool ExecuteWithLoopDetection()
+        {
+            var pointer = new HashSet<int>();
+
+            Data.Reset();
+            while (Data.Running)
+            {
+                Data.ExecuteNextInstruction();
+                if (pointer.Contains(Data.ExecutionPointer))
+                    return false;
+
+                pointer.Add(Data.ExecutionPointer);
+            }
+
+            return true;
         }
 
         protected override object Solve1()
         {
-            throw new Exception("Solver error");
+            ExecuteWithLoopDetection();
+            return Data.GobalValue;
         }
 
         protected override object Solve2()
         {
+            for (var i = 0; i < Data.Program.Count; i++)
+            {
+                var instruction = Data.Program[i];
+                if (instruction.Operation == "jmp")
+                    Data.Program[i] = instruction with { Operation = "nop" };
+                else if (instruction.Operation == "nop" && instruction.Argument != 0)
+                    Data.Program[i] = instruction with { Operation = "jmp" };
+                else
+                    continue;
+
+                if (ExecuteWithLoopDetection())
+                    return Data.GobalValue;
+
+                Data.Program[i] = instruction;
+            }
+
             throw new Exception("Solver error");
         }
     }
