109dcaaaf8668372417f560fabae509aed3f6e95
Volkmar Rigo <VolkmarR@users.noreply.github.com>
2020-12-10 21:55:15 +0100

    Fix day 8 (#13)

--- Base/DayInitialization.cs
+++ Base/DayInitialization.cs
@@ -18,7 +18,10 @@ namespace AdventOfCode.Base
             var dayDir = projectDir + @"\Day" + dayStr + "\\";
 
             if (Directory.Exists(dayDir))
+            {
                 Console.WriteLine($"Directory {dayDir} was already initialized");
+                return;
+            }
 
             Directory.CreateDirectory(dayDir);
 
