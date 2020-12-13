83a402a130db7789763a57935e9ee0295304e599
Volkmar Rigo <volkmar@rigo.bz>
2020-12-13 13:59:48 +0100

    Part 2 Brute force

--- Day13/Day13Solver.cs
+++ Day13/Day13Solver.cs
@@ -23,35 +23,65 @@ namespace Day13
     public class Day13Solver : SolverBase
     {
         int EarliestDepartTime;
-        List<int?> Data;
+        List<short> Data;
+        List<short> DataOffset;
 
         protected override void Parse(List<string> data)
         {
             EarliestDepartTime = int.Parse(data[0]);
-            Data = data[1].Split(",").Select(q => q == "x" ? null : (int?)int.Parse(q)).ToList();
+            Data = new List<short>();
+            DataOffset = new List<short>();
+            short offset = 0;
+            var DataForSorting = new List<(short time, short offset)>();
+            foreach (var item in data[1].Split(","))
+            {
+                if (item != "x")
+                    DataForSorting.Add((short.Parse(item), offset));
+                offset++;
+            }
+
+            DataForSorting.OrderByDescending(q => q.time);
+            Data = DataForSorting.Select(q => q.time).ToList();
+            DataOffset = DataForSorting.Select(q => q.offset).ToList();
         }
 
-        int? NextStartTime(int? busID)
+        static long NextStartTime(int busID, long timeStamp)
         {
-            if (busID == null)
-                return null;
-
-            int result = busID.Value * (EarliestDepartTime / busID.Value);
-            if (result < EarliestDepartTime)
-                result += busID.Value;
+            var result = busID * (timeStamp / busID);
+            if (result < timeStamp)
+                result += busID;
             return result;
         }
 
         protected override object Solve1()
         {
-            var nextStart = Data.Select(NextStartTime).ToList();
-            var best = nextStart.Where(q => q.HasValue).Min();
+            var nextStart = Data.Select(q => NextStartTime(q, EarliestDepartTime)).ToList();
+            var best = nextStart.Where(q => q > -1).Min();
             return (best - EarliestDepartTime) * Data[nextStart.IndexOf(best)];
         }
 
         protected override object Solve2()
         {
-            throw new Exception("Solver error");
+            // long startTimeStamp = NextStartTime(Data[0], 1013728);
+            long startTimeStamp = NextStartTime(Data[0], 100_000_000_000_000);
+            var counters = new long[Data.Count];
+            for (var i = 0; i < Data.Count; i++)
+                counters[i] = NextStartTime(Data[i], startTimeStamp) - DataOffset[i] - startTimeStamp;
+
+            var notAllEqual = true;
+            while (notAllEqual)
+            {
+                counters[0] += Data[0];
+                notAllEqual = false;
+                for (int i = 1; i < counters.Length && !notAllEqual; i++)
+                {
+                    while (counters[i] < counters[0])
+                        counters[i] += Data[i];
+                    notAllEqual = counters[0] != counters[i];
+                }
+            }
+
+            return counters[0] + startTimeStamp;
         }
     }
 }
