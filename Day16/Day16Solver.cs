097a3a25eccd0db10b167af2e5c480df2ad79947
Volkmar Rigo <volkmar@rigo.bz>
2020-12-16 08:35:44 +0100

    part 2 unfinished

--- Day16/Day16Solver.cs
+++ Day16/Day16Solver.cs
@@ -15,12 +15,12 @@ namespace Day16
 
         [Fact]
         public void RunStep1() => Output.WriteLine(new Day16Solver().ExecutePuzzle1());
-        
+
         [Fact]
         public void RunStep2() => Output.WriteLine(new Day16Solver().ExecutePuzzle2());
     }
 
-    public record RuleBetween (int start, int end);
+    public record RuleBetween(int start, int end);
     public record Rule(string name, RuleBetween[] rules);
     public record Ticket(int[] numbers);
 
@@ -64,11 +64,12 @@ namespace Day16
             ParseTickets(data.SkipWhile(NotEmptyLine).Skip(1).SkipWhile(NotEmptyLine).Skip(1));
         }
 
+        List<RuleBetween> GetAllRulesBetween() => Rules.SelectMany(q => q.rules, (a, b) => b).ToList();
+
         protected override object Solve1()
         {
             var invalidNumbers = new List<int>();
-            var rulesBetween = Rules.SelectMany(q => q.rules, (a, b) => b).ToList();
-
+            var rulesBetween = GetAllRulesBetween();
 
             foreach (var item in Tickets)
             {
@@ -80,8 +81,46 @@ namespace Day16
             return invalidNumbers.Sum();
         }
 
+        void RemoveInvalidTickets()
+        {
+            var rulesBetween = GetAllRulesBetween();
+            var i = 0;
+            while (i < Tickets.Count)
+                if (Tickets[i].numbers.Any(q => !rulesBetween.Any(s => q >= s.start && q <= s.end)))
+                    Tickets.RemoveAt(i);
+                else
+                    i++;
+        }
+
         protected override object Solve2()
         {
+            RemoveInvalidTickets();
+
+            var RuleForRow = new Dictionary<int, Rule>();
+            var NumberRows = new List<List<int>>();
+            for (int i = 0; i < Tickets[0].numbers.Length; i++)
+                NumberRows.Add(Tickets.Select(q => q.numbers[i]).ToList());
+
+            var rules = Rules.ToList();
+
+            while (rules.Count > 0)
+            {
+                var match = new List<int>();
+                for (int i = 0; i < NumberRows.Count; i++)
+                    if (!RuleForRow.ContainsKey(i))
+                    {
+
+                    }
+
+
+
+
+            }
+
+
+
+
+
             throw new Exception("Solver error");
         }
     }
